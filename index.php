406f4c1c86c8230ea31cf575f3786e180be4b77a
James Meisler <james@mindfulware.com>
2013-10-14 11:49:26 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+	'templates.path' => 'templates'
+));
+
+$app->config(array(
+	'templates.path' => './',
+));
+
+//$file = '/Users/james/htdocs/freelance/scrape/marshbills.html';
+//$handle = fopen($file, "r");
+//$contents = fread($handle, filesize($file));
+//fclose($handle);
+
+
+//$pattern = '/SB\w{1,3}-(int|eng|enr)\.pdf/';
+
+//preg_match_all($pattern, $contents, $matches);
+
+//print_r($matches[0]);
+
+$app->get('/', function() use ($app){
+	$app->render('form.html');
+});
+
+$app->post('/scrape', function()use($app){
+	$file = $_FILES['filename']['tmp_name'];
+
+	$handle = fopen($file, "r");
+	$contents = fread($handle, filesize($file));
+	fclose($handle);
+
+
+	$pattern = '/SB\w{1,3}-(int|eng|enr)\.pdf/';
+
+	preg_match_all($pattern, $contents, $matches);
+
+	$out = '';
+
+	foreach($matches[0] as $match){
+		 $out .= $match . "\n";
+	}
+
+	echo $out;
+});
+
+
+$app->run()
+?>
